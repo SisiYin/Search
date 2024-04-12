fa0a8924b5c9ed378e11ba32fc7b29cfedbe2292
SisiYin <t3chyi00@students.oamk.fi>
2024-04-13 01:01:47 +0300

    first commit

---
+++ Client/input.js
@@ -0,0 +1,85 @@
+//window.history.pushState(null, '', '/foodhub');
+const BACK_END_URL = 'http://localhost:3001'
+
+
+const searchList = document.querySelector('ul')
+const searchInput = document.getElementById('searchInput')
+
+const keyword = searchInput.value.trim();
+const searchResultsUrl = 'http://127.0.0.1:5500/Client/input.html/searh?keyword='+keyword;
+
+const searchResult = async()=>{
+  
+  try {
+    const response = await fetch (BACK_END_URL + '/search?keyword='+ keyword);
+    
+    const searchResults = await response.json();
+    searchResults. forEach(searchResult => {
+      const searchItem = document.createElement('li');
+      const author = document.createElement('div');
+      author.textContent = searchResult.username;
+      const title = document.createElement('div');
+      title.textContent = searchResult.title;
+      const description = document.createElement('div')
+      description.textContent = searchResult.description;
+      const rate = document.createElement('div')
+      rate.textContent = searchResult.rate;
+      
+      searchItem.appendChild(title);
+      searchItem.appendChild(author);
+      searchItem.appendChild(description);
+      searchItem.appendChild(rate);
+      
+      searchList.appendChild(searchItem);
+    })
+
+  } catch (error) {
+    alert("Error retrieving tasks" + error.message)
+  }
+}
+
+
+searchInput.addEventListener('keypress',async (event) =>{
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    //const keyword = searchInput.value.trim();
+    // const searchResultsUrl = `http://127.0.0.1:5500/Client/input.html/search?keyword=${keyword}`;
+    // window.location.href = searchResultsUrl;
+    searchResult();
+    //searchInput.value = '';
+    //searchInput.focus();
+    // addCommentToPage(commentInput.value);
+  // } catch (error) {
+  //   console.error(error);
+    //window.location.href = 'searchResult.html';
+  } 
+})
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const categoryIcons = document.querySelectorAll('.category-icons img');
+//   const searchResults = document.querySelector('.search-results');
+
+//   categoryIcons.forEach(icon => {
+//     icon.addEventListener('click', async () => {
+//       const category = icon.getAttribute('data-category');
+//       try {
+//         const response = await fetch(`/recipes?category=${category}`);
+//         const recipes = await response.json();
+//         displayRecipes(recipes);
+//       } catch (error) {
+//         console.error('Error fetching recipes', error);
+//       }
+//     });
+//   });
+
+//   function displayRecipes(recipes) {
+//     searchResults.innerHTML = '';
+//     recipes.forEach(recipe => {
+//       const li = document.createElement('li');
+//       li.textContent = `${recipe.name} - ${recipe.category}`;
+//       searchResults.appendChild(li);
+//     });
+//   }
+// });
---
+++ server/input.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+const port = 3001;
+
+const openDb = () => {
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'comments',
+    password: '251423',
+    port: 5432
+  })
+  return pool
+}
+
+app.get("/",(req,res) => {
+  const pool = openDb()
+
+  pool.query('select * from post',(error,result) => {
+    if (error) {
+      res.status(500).json({error:error.message})
+    }
+    res.status(200).json(result.rows)
+  })
+})
+
+// app.get("/search/:keyword",async(req,res)=>{
+//   const pool = openDb();
+//   const keyword = req.params.keyword;
+
+//   try{
+//     const result = await pool.query('SELECT * FROM post WHERE description like $1',['%' + keyword + '%']);
+//     const searchResults = result.rows;
+
+//     res.status(200).json(searchResults);
+//   } catch (error){
+//     res.status(500).json({error: error.message})
+//   }
+// })
+
+
+app.get("/search",async(req,res)=>{
+  const pool = openDb();
+  //const keyword = '3';
+  const keyword = req.query.keyword;
+  
+  try{
+    const result = await pool.query('Select a.username,p.title,p.rate,p.photo_data,p.description from ( SELECT * FROM post WHERE description ilike $1) p join account a on a.account_id = p.account_id',['%' + keyword + '%']);
+    const searchResults = result.rows;
+
+    res.status(200).json(searchResults);
+  } catch (error){
+    res.status(500).json({error: error.message})
+  }
+})
+
+app.listen(port);
